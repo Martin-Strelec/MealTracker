d80e055eef898e0ab40e1d5c04354649855cccac
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-10 20:31:07 +0000

    feat: Add navigation drawer
    Adds a `ModalNavigationDrawer` to the main screen, allowing users to navigate between Home, Favourites, and Tracking screens.
    
    - Updates the `AppScreen` enum to include icons and a flag to show/hide items from the drawer.
    - Adds a menu icon to the top app bar to open the drawer.
    - The `AddMeal` screen is excluded from the drawer menu.

--- app/src/main/java/com/example/mealtracker/MainActivity.kt
+++ app/src/main/java/com/example/mealtracker/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.mealtracker
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,9 +34,18 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mealtracker.ui.theme.AppTheme
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.mealtracker.ui.theme.TWEEN_24
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +59,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-enum class AppScreen(@StringRes val title: Int) {
-    Home(title = R.string.app_name),
-    AddMeal(title = R.string.add_meal),
-    Favourites(title = R.string.favourites),
-    Tracking(title = R.string.tracking)
+enum class AppScreen(
+    @StringRes val title: Int,
+    val icon: ImageVector,
+    val showInDrawer: Boolean = true,
+    ) {
+    Home(title = R.string.meals, Icons.Default.Home),
+    AddMeal(title = R.string.add_meal, Icons.Default.Add, showInDrawer = false),
+    Favourites(title = R.string.favourites, Icons.Default.Favorite),
+    Tracking(title = R.string.tracking, Icons.Default.Star)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,6 +76,7 @@ fun AppBar(
     currentScreen: AppScreen,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
+    onMenuClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
@@ -70,6 +95,13 @@ fun AppBar(
                         contentDescription = stringResource(R.string.back_button)
                     )
                 }
+            } else {
+                IconButton(onClick = onMenuClicked) {
+                    Icon (
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = stringResource(R.string.menu_button)
+                    )
+                }
             }
         }
     )
@@ -80,37 +112,72 @@ fun AppBar(
 fun MealTrackerApp(
     navController: NavHostController = rememberNavController()
 ) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
     val backStackEntry by navController.currentBackStackEntryAsState()
     val currentScreen = AppScreen.valueOf(
         backStackEntry?.destination?.route?: AppScreen.Home.name)
-    Scaffold(
-        topBar = {
-            AppBar(
-                canNavigateBack = navController.previousBackStackEntry != null,
-                currentScreen = currentScreen,
-                navigateUp = { navController.navigateUp() }
-            )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(modifier = Modifier.padding(TWEEN_24)) {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                AppScreen.entries.filter{it.showInDrawer}.forEach { screen ->
+                    NavigationDrawerItem(
+                        icon = { Icon(screen.icon, contentDescription = null)},
+                        label = { Text(stringResource(screen.title))},
+                        selected = currentScreen == screen,
+                        onClick = {
+                            scope.launch {
+                                drawerState.close()
+                            }
+                            navController.navigate(screen.name)
+                        }
+                    )
+                }
+            }
         }
     ) {
-        innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = AppScreen.Home.name,
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable(route = AppScreen.Home.name) {
-                HomeScreen(
-                    onRecipeAddClicked = { navController.navigate(AppScreen.AddMeal.name) }
+        Scaffold(
+            topBar = {
+                AppBar(
+                    canNavigateBack = navController.previousBackStackEntry != null,
+                    currentScreen = currentScreen,
+                    navigateUp = { navController.navigateUp() },
+                    onMenuClicked = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }
                 )
             }
-            composable(route = AppScreen.AddMeal.name) {
-                AddMealScreen()
-            }
-            composable(route = AppScreen.Favourites.name) {
-                FavouritesScreen()
-            }
-            composable(route = AppScreen.Tracking.name) {
-                TrackingScreen()
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = AppScreen.Home.name,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(route = AppScreen.Home.name) {
+                    HomeScreen(
+                        onRecipeAddClicked = { navController.navigate(AppScreen.AddMeal.name) }
+                    )
+                }
+                composable(route = AppScreen.AddMeal.name) {
+                    AddMealScreen()
+                }
+                composable(route = AppScreen.Favourites.name) {
+                    FavouritesScreen()
+                }
+                composable(route = AppScreen.Tracking.name) {
+                    TrackingScreen()
+                }
             }
         }
     }
