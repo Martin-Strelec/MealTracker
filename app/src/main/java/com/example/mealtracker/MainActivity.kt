c15c87cb09e84708ada8001cfd965bf01a0fe15a
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-10 17:58:38 +0000

    feat: Add navigation and AppBar
    Introduces basic navigation structure using Jetpack Compose Navigation.
    
    - Defines `AppScreen` enum for different app destinations.
    - Creates a reusable `AppBar` with a back button.
    - Sets up `NavHost` to navigate between Home, AddMeal, Favourites, and Tracking screens.

--- app/src/main/java/com/example/mealtracker/MainActivity.kt
+++ app/src/main/java/com/example/mealtracker/MainActivity.kt
@@ -4,35 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mealtracker.ui.theme.AppTheme
-import com.example.mealtracker.ui.theme.TWEEN_24
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,8 +33,87 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                MealTrackerScreen()
+                MealTrackerApp()
                 }
             }
         }
     }
+
+enum class AppScreen(@StringRes val title: Int) {
+    Home(title = R.string.app_name),
+    AddMeal(title = R.string.add_meal),
+    Favourites(title = R.string.favourites),
+    Tracking(title = R.string.tracking)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    currentScreen: AppScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        // You can add navigation actions or other items here
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MealTrackerApp(
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = AppScreen.valueOf(
+        backStackEntry?.destination?.route?: AppScreen.Home.name)
+    Scaffold(
+        topBar = {
+            AppBar(
+                canNavigateBack = navController.previousBackStackEntry != null,
+                currentScreen = currentScreen,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) {
+        innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = AppScreen.Home.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = AppScreen.Home.name) {
+                HomeScreen(
+                    onRecipeAddClicked = { navController.navigate(AppScreen.AddMeal.name) }
+                )
+            }
+            composable(route = AppScreen.AddMeal.name) {
+                AddMealScreen()
+            }
+            composable(route = AppScreen.Favourites.name) {
+                FavouritesScreen()
+            }
+            composable(route = AppScreen.Tracking.name) {
+                TrackingScreen()
+            }
+        }
+    }
+
+}
+
