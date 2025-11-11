b44a39a4e0a64682489a54ca7086ec070440c40a
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-11 20:10:37 +0000

    Added Meal data class

---
+++ app/src/main/java/com/example/mealtracker/data/Meal.kt
@@ -0,0 +1,15 @@
+package com.example.mealtracker.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Meal(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val image: String,
+    val description: String,
+    val calories: Int,
+    val dateAdded: Long
+)
