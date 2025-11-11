d4dfc8942595dffe6ee64cb06c4c7645a954fb8e
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-11 20:14:54 +0000

    feat: Add MealDao interface
    Defines the Data Access Object (DAO) for the `Meal` entity. It includes functions for upserting, deleting, and querying meals. Queries allow fetching meals ordered by date or name, and retrieving a specific meal by its ID.

---
+++ app/src/main/java/com/example/mealtracker/data/MealDao.kt
@@ -0,0 +1,25 @@
+package com.example.mealtracker.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MealDao {
+    @Upsert
+    suspend fun upsertMeal(meal: Meal)
+
+    @Delete
+    suspend fun deleteMeal(meal: Meal)
+
+    @Query("SELECT * FROM meal ORDER BY dateAdded")
+    fun getMealsOrderedByDate(): Flow<List<Meal>>
+
+    @Query("SELECT * FROM meal ORDER BY name")
+    fun getMealsOrderedByName(): Flow<List<Meal>>
+
+    @Query("SELECT * FROM meal WHERE id = :id")
+    suspend fun getMealById(id: Int): Meal?
+}
