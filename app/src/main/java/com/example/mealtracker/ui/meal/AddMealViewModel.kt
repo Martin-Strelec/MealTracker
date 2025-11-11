8402efc26ca36ead70620a7f3497c4bbd78d8ac7
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-11 21:07:24 +0000

    feat: Add ViewModel for adding meals
    Introduces `AddMealViewModel` to manage the state and logic for adding a new meal.
    
    This includes:
    - `MealUiState` and `MealDetails` data classes to represent the UI state.
    - Input validation for meal name, calories, and description.
    - Functions to save a meal to the repository.
    - Mappers to convert between `Meal` and `MealDetails`.

--- app/src/main/java/com/example/mealtracker/ui/meal/AddMealViewModel.kt
+++ app/src/main/java/com/example/mealtracker/ui/meal/AddMealViewModel.kt
@@ -1,2 +1,65 @@
 package com.example.mealtracker.ui.meal
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.mealtracker.data.Meal
+import com.example.mealtracker.data.MealsRepository
+
+class AddMealViewModel(private val mealRepository: MealsRepository): ViewModel() {
+    var mealUiState by mutableStateOf(MealUiState())
+        private set
+
+    fun updateUiState(mealDetails: MealDetails) {
+        mealUiState =
+            MealUiState(mealDetails = mealDetails, isEntryValid = validateInput(mealDetails))
+    }
+
+    suspend fun saveMeal() {
+        if (validateInput()) {
+            mealRepository.upsertMeal(mealUiState.mealDetails.toMeal())
+        }
+    }
+
+    private fun validateInput(uiState: MealDetails = mealUiState.mealDetails): Boolean {
+        return with(uiState) {
+            name.isNotBlank() && calories != 0 && description.isNotBlank()
+        }
+    }
+}
+
+/**
+ * Represents Ui State for an Item.
+ */
+data class MealUiState(
+    val mealDetails: MealDetails = MealDetails(),
+    val isEntryValid: Boolean = false
+)
+
+data class MealDetails(
+    val id: Int = 0,
+    val name: String = "",
+    val image: String = "",
+    val description: String = "",
+    val calories: Int = 0,
+    val dateAdded: Long = System.currentTimeMillis()
+)
+
+fun MealDetails.toMeal(): Meal = Meal(
+    id = id,
+    name = name,
+    image = "",
+    description = description,
+    calories = calories,
+    dateAdded = System.currentTimeMillis()
+)
+
+fun Meal.toMealDetails(): MealDetails = MealDetails(
+    id = id,
+    name = name,
+    image = "",
+    description = description,
+    calories = calories,
+    dateAdded = System.currentTimeMillis()
+)
