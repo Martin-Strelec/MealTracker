438595bfcffd5a0ae4508d8af61828ddcce3f6a1
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-14 15:06:14 +0000

    feat: Enhance NavigationDestination
    Adds `icon` and `showInDrawer` properties to the `NavigationDestination` interface to support drawer navigation.

--- app/src/main/java/com/example/mealtracker/ui/navigation/NavigationDestination.kt
+++ app/src/main/java/com/example/mealtracker/ui/navigation/NavigationDestination.kt
@@ -1,7 +1,10 @@
 package com.example.mealtracker.ui.navigation
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 interface NavigationDestination {
     val route: String
-
     val titleRes: Int
+    val icon: ImageVector
+    val showInDrawer: Boolean
 }
