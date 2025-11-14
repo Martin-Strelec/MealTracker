a26a0a77c24f1f1ebb401ffcb75da041eeee35ac
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-14 15:05:10 +0000

    feat: Add Favourites and Tracking screens to NavGraph
    Adds the `FavouritesScreen` and `TrackingScreen` to the navigation graph. This change also comments out the previous routes for editing meals.

--- app/src/main/java/com/example/mealtracker/ui/navigation/MealTrackerNavGraph.kt
+++ app/src/main/java/com/example/mealtracker/ui/navigation/MealTrackerNavGraph.kt
@@ -1,20 +1,28 @@
 package com.example.mealtracker.ui.navigation
 
+import android.R.attr.type
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.mealtracker.ui.favourites.FavouriteDestination
+import com.example.mealtracker.ui.favourites.FavouritesScreen
 import com.example.mealtracker.ui.home.HomeDestination
 import com.example.mealtracker.ui.home.HomeScreen
 import com.example.mealtracker.ui.meal.AddMealDestination
 import com.example.mealtracker.ui.meal.AddMealScreen
 import com.example.mealtracker.ui.meal.EditMealDestination
+import com.example.mealtracker.ui.meal.EditMealScreen
+import com.example.mealtracker.ui.tracked.TrackingDestination
+import com.example.mealtracker.ui.tracked.TrackingScreen
 
 @Composable
 fun MealTrackerNavHost(
-    navController: NavController,
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     NavHost(
@@ -31,27 +39,33 @@ fun MealTrackerNavHost(
                 onNavigateUp = { navController.navigateUp()
                 })
         }
-        composable(
-            route = EditMealDestination.routeWithArgs,
-            arguments = listOf(navArgument(MealDetailsDestination.itemIdArg) {
-                type = NavType.IntType
-            })
-        ) {
-            MealDetailsScreen(
-                navigateToEditMeal = {
-                    navController.navigate("${MealUpdateDestination.route}/$it")
-                },
-                navigateBack = { navController.navigateUp() }
-            )
+        composable(route = FavouriteDestination.route) {
+            FavouritesScreen()
         }
-        composable(
-            route = MealUpdateDestination.routeWithArgs,
-            arguments = listOf(navArgument(MealUpdateDestination.itemIdArg) {
-                type = NavType.IntType
-            })
-        ) {
-            EditMealScreen(navigateBack = { navController.popBackStack() },
-                onNavigateUp = { navController.navigateUp() })
+        composable(route = TrackingDestination.route) {
+            TrackingScreen()
         }
+//        composable(
+//            route = EditMealDestination.routeWithArgs,
+//            arguments = listOf(navArgument(MealDetailsDestination.itemIdArg) {
+//                type = NavType.IntType
+//            })
+//        ) {
+//            MealDetailsScreen(
+//                navigateToEditMeal = {
+//                    navController.navigate("${MealUpdateDestination.route}/$it")
+//                },
+//                navigateBack = { navController.navigateUp() }
+//            )
+//        }
+//        composable(
+//            route = EditMealDestination.routeWithArgs,
+//            arguments = listOf(navArgument(MealUpdateDestination.itemIdArg) {
+//                type = NavType.IntType
+//            })
+//        ) {
+//            EditMealScreen(navigateBack = { navController.popBackStack() },
+//                onNavigateUp = { navController.navigateUp() })
+//        }
     }
 }
--- app/src/main/java/com/example/mealtracker/ui/tracked/TrackingScreen.kt
+++ app/src/main/java/com/example/mealtracker/ui/tracked/TrackingScreen.kt
@@ -2,13 +2,27 @@ package com.example.mealtracker.ui.tracked
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.mealtracker.R
+import com.example.mealtracker.ui.navigation.NavigationDestination
+
+object TrackingDestination : NavigationDestination {
+    override val route = "tracking"
+    override val titleRes = R.string.tracking
+    override val icon = Icons.Filled.Star
+    override val showInDrawer = true
+    override val topLevel = true
+}
 
 @Composable
-fun TrackingScreen(modifier: Modifier = Modifier) {
+fun TrackingScreen(
+    modifier: Modifier = Modifier) {
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
