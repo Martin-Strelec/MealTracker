55aa8e8742b12ba12db4c4b18062b7bde617382e
Martin-Strelec <m.strelec2021@gmail.com>
2025-11-15 14:03:23 +0000

    feat: Add ViewModel CreationExtras extension
    Adds a `CreationExtras` extension function to get the `MealTrackerApplication` instance. This simplifies accessing the application context within ViewModel factories.

--- app/src/main/java/com/example/mealtracker/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/mealtracker/ui/AppViewModelProvider.kt
@@ -1,8 +1,9 @@
 package com.example.mealtracker.ui
 
-import androidx.sqlite.db.SupportSQLiteOpenHelper
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import com.example.mealtracker.ui.meal.EditMealViewModel
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.mealtracker.MealTrackerApplication
@@ -25,3 +26,10 @@ object AppViewModelProvider {
         }
     }
 }
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [MealTrackerApplication].
+ */
+fun CreationExtras.MealTrackerApplication(): MealTrackerApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as MealTrackerApplication)
