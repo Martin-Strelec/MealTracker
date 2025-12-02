8d4349c81ece73966fa2b047963029f9fc78f975
Martin-Strelec <m.strelec2021@gmail.com>
2025-12-02 11:27:06 +0000

    Added navigation to meal detail

--- app/src/main/java/com/example/mealtracker/ui/navigation/MealTrackerNavGraph.kt
+++ app/src/main/java/com/example/mealtracker/ui/navigation/MealTrackerNavGraph.kt
@@ -1,6 +1,8 @@
 package com.example.mealtracker.ui.navigation
 
 import android.R.attr.type
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -17,9 +19,12 @@ import com.example.mealtracker.ui.meal.AddMealDestination
 import com.example.mealtracker.ui.meal.AddMealScreen
 import com.example.mealtracker.ui.meal.EditMealDestination
 import com.example.mealtracker.ui.meal.EditMealScreen
+import com.example.mealtracker.ui.meal.MealDetailsDestination
+import com.example.mealtracker.ui.meal.MealDetailsScreen
 import com.example.mealtracker.ui.tracked.TrackingDestination
 import com.example.mealtracker.ui.tracked.TrackingScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MealTrackerNavHost(
     navController: NavHostController,
@@ -29,43 +34,46 @@ fun MealTrackerNavHost(
         navController = navController, startDestination = HomeDestination.route, modifier = modifier
     ) {
         composable(route = HomeDestination.route) {
-            HomeScreen(navigateToAddMeal = { navController.navigate(AddMealDestination.route) },
-                navigateToMealUpdate = {
-                    navController.navigate("${EditMealDestination.route}/$it")
-                })
+            HomeScreen(
+                navigateToAddMeal = { navController.navigate(AddMealDestination.route) },
+                navigateToMealDetail = {
+                    navController.navigate("${MealDetailsDestination.route}/$it")
+                }
+            )
         }
         composable(route = AddMealDestination.route) {
-            AddMealScreen(navigateBack = { navController.popBackStack() },
-                onNavigateUp = { navController.navigateUp()
-                })
-        }
-        composable(route = FavouriteDestination.route) {
-            FavouritesScreen()
+            AddMealScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
         }
         composable(route = TrackingDestination.route) {
             TrackingScreen()
         }
-//        composable(
-//            route = EditMealDestination.routeWithArgs,
-//            arguments = listOf(navArgument(MealDetailsDestination.itemIdArg) {
-//                type = NavType.IntType
-//            })
-//        ) {
-//            MealDetailsScreen(
-//                navigateToEditMeal = {
-//                    navController.navigate("${MealUpdateDestination.route}/$it")
-//                },
-//                navigateBack = { navController.navigateUp() }
-//            )
-//        }
-//        composable(
-//            route = EditMealDestination.routeWithArgs,
-//            arguments = listOf(navArgument(MealUpdateDestination.itemIdArg) {
-//                type = NavType.IntType
-//            })
-//        ) {
-//            EditMealScreen(navigateBack = { navController.popBackStack() },
-//                onNavigateUp = { navController.navigateUp() })
-//        }
+        composable(route = FavouriteDestination.route) {
+            FavouritesScreen()
+        }
+        composable(
+            route = MealDetailsDestination.routeWithArgs,
+            arguments = listOf(navArgument(MealDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            MealDetailsScreen(
+                navigateToEditMeal = { navController.navigate("${EditMealDestination.route}/$it") },
+                navigateBack = { navController.navigateUp() }
+            )
+        }
+        composable(
+            route = EditMealDestination.routeWithArgs,
+            arguments = listOf(navArgument(EditMealDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EditMealScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
     }
 }
