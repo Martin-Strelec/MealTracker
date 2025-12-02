27c7a7da48175c95874ce507bb58be803a6b51f8
Martin-Strelec <m.strelec2021@gmail.com>
2025-12-02 11:26:17 +0000

    Added MealDetailsScreen.kt and MealDetailsViewModel.kt

---
+++ app/src/main/java/com/example/mealtracker/ui/meal/MealDetailsViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.mealtracker.ui.meal
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mealtracker.data.MealsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class MealDetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mealsRepository: MealsRepository
+) : ViewModel() {
+
+    private val mealId: Int = checkNotNull(savedStateHandle[MealDetailsDestination.itemIdArg])
+
+    val uiState: StateFlow<MealDetailsUiState> =
+        mealsRepository.getMealStream(mealId)
+            .filterNotNull()
+            .map{
+                MealDetailsUiState(mealDetails = it.toMealDetails())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = MealDetailsUiState()
+            )
+
+    suspend fun deleteMeal() {
+        mealsRepository.deleteMeal(uiState.value.mealDetails.toMeal())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+data class MealDetailsUiState(
+    val mealDetails: MealDetails = MealDetails()
+)
