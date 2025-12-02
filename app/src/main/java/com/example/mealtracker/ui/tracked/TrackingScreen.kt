c8642712119668a77ab89c0c1f832b42adee0fe0
Martin-Strelec <m.strelec2021@gmail.com>
2025-12-02 19:51:20 +0000

    Copied over HomeScreen elements

--- app/src/main/java/com/example/mealtracker/ui/tracked/TrackingScreen.kt
+++ app/src/main/java/com/example/mealtracker/ui/tracked/TrackingScreen.kt
@@ -1,17 +1,40 @@
 package com.example.mealtracker.ui.tracked
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mealtracker.R
+import com.example.mealtracker.ui.AppViewModelProvider
+import com.example.mealtracker.ui.home.HomeBody
 import com.example.mealtracker.ui.navigation.NavigationDestination
+import com.example.mealtracker.ui.theme.TWEEN_24
 
 object TrackingDestination : NavigationDestination {
     override val route = "tracking"
@@ -20,13 +43,48 @@ object TrackingDestination : NavigationDestination {
     override val showInDrawer = true
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TrackingScreen(
-    modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Tracking Screen")
+    navigateToMealDetail: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: TrackingViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val homeUiState by viewModel.trackingUiState.collectAsState()
+    var searchQuery by remember { mutableStateOf("")}
+
+    Scaffold { innerPadding ->
+        Surface (
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ){
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                //Your main content, like a list of meals, will go here
+                //For now, let's put our search bar inside
+                TextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = TWEEN_24),
+                    placeholder = { Text("Search for a meal...") },
+                    leadingIcon = {
+                        Icon(Icons.Filled.Search, contentDescription = "Search Icon")
+                    },
+                    singleLine = true
+                )
+                HomeBody(
+                    mealList = homeUiState.mealList,
+                    onMealClick = navigateToMealDetail,
+                    emptyText = stringResource(R.string.no_tracked_meals_description),
+                    modifier = Modifier
+                )
+            }
+        }
     }
 }
