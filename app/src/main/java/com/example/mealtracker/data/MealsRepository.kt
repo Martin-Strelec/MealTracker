233c1f0a418abdf644000879d1ca1b758c7e6a60
Martin-Strelec <m.strelec2021@gmail.com>
2025-12-03 19:18:31 +0000

    Added functions to support adding and removing meals from tracked

--- app/src/main/java/com/example/mealtracker/data/MealsRepository.kt
+++ app/src/main/java/com/example/mealtracker/data/MealsRepository.kt
@@ -8,12 +8,13 @@ interface MealsRepository {
 
     fun getMealsOrderedByNameStream(): Flow<List<Meal>>
 
+    fun getAllTrackedMeals(): Flow<List<TrackedMealEntry>>
+
     fun getFavouriteMeals(): Flow<List<Meal>>
 
-    fun getTrackedMeals(): Flow<List<Meal>>
-
     fun getMealStream(id: Int): Flow<Meal?>
-
+    suspend fun insertTrackedMeal(mealId: Int, date: Long)
+    suspend fun deleteTrackedMeal(trackId: Int, mealId: Int, date: Long)
     suspend fun upsertMeal(meal: Meal)
 
     suspend fun deleteMeal(meal: Meal)
